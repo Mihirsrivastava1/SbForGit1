e805a08320e043a85739382ab95bb28fca885c15
Mihirsrivastava1 <mihirsri234@gmail.com>
2024-12-09 15:50:07 +0530

    1. Added a read me file 2. Created Student class with fields id and name, and a constructor and toString. 3. Modified GitDemo to test Student class

--- src/sb/for_git/GitDemo.java
+++ src/sb/for_git/GitDemo.java
@@ -1,10 +1,14 @@
 package sb.for_git;
 
+import sb.for_git.ex_1.Student;
+
 public class GitDemo {
 
 	public static void main(String[] args) {
 		System.out.println("\n This project is to learn GIT");
 
+		Student student = new Student(101,"Mihir");
+		System.out.println(student);
 	}
 
 }
